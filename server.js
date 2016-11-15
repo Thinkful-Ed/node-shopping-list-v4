c5bbb35efa520697ac3101d204cd1fda6b47ac79
Benjamin White <benjamin.e.white1@gmail.com>
2016-11-15 15:45:17 -0500

    updated

--- server.js
+++ server.js
@@ -1,47 +1,39 @@
+
 const express = require('express');
+const router = express.Router();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const {ShoppingList, Recipes} = require('./models');
+
+const jsonParser = bodyParser.json();
 const app = express();
 
-const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json();
+// log the http layer
+app.use(morgan('common'));
 
-const {storage} = require('./storage');
-
-
-// we're going to add some items to storage
+// we're going to add some items to ShoppingList
 // so there's some data to look at
-storage.add('beans', 2.5);
-storage.add('tomatoes', 5);
-storage.add('peppers', 3);
+ShoppingList.create('beans', true);
+ShoppingList.create('tomatoes', false);
+ShoppingList.create('peppers', false);
 
-app.get('/items', (req, res) => {
-  res.json(storage.getItems());
+// adding some recipes to `Recipes` so there's something
+// to retrieve.
+Recipes.create(
+  'boiled white rice', ['1 cup white rice', '2 cups water', 'pinch of salt']);
+Recipes.create(
+  'milkshake', ['2 tbsp cocoa', '2 cups vanilla ice cream', '1 cup milk']);
+
+// when the root of this router is called with GET, return
+// all current ShoppingList items
+app.get('/shopping-list', (req, res) => {
+  res.json(ShoppingList.get());
 });
 
-app.post('/items', jsonParser, (req, res) => {
+app.post('/shopping-list', jsonParser, (req, res) => {
   // ensure `name` and `budget` are in request body
   const requiredFields = ['name', 'budget'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body` 
-      console.error(message);
-      return res.status(400).send(message);
-    }
-  }
-
-  const item = storage.add({name: req.body.name, budget: req.body.budget});
-  res.status(201).json(item);
-});
-
-
-app.delete('/items/:id', (req, res) => {
-  storage.deleteItem(req.params.id);
-  console.log(`Deleted shopping list item \`${req.params.ID}\``);
-  res.status(204).end();
-});
-
-app.put('/items/:id', jsonParser, (req, res) => {
-  const requiredFields = ['name', 'budget', 'id'];
   for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
@@ -50,21 +42,101 @@ app.put('/items/:id', jsonParser, (req, res) => {
       return res.status(400).send(message);
     }
   }
-  if (req.params.id !== id) {
+
+  const item = ShoppingList.create(
+    {name: req.body.name, budget: req.body.budget});
+  res.status(201).json(item);
+});
+
+// when PUT request comes in with updated item, ensure has
+// required fields. also ensure that item id in url path, and
+// item id in updated item object match. if problems with any
+// of that, log error and send back status code 400. otherwise
+// call `ShoppingList.update` with updated item.
+app.put('/shopping-list/:id', jsonParser, (req, res) => {
+  const requiredFields = ['name', 'checked', 'id'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
     const message = (
       `Request path id (${req.params.id}) and request body id `
       `(${req.body.id}) must match`);
     console.error(message);
     return res.status(400).send(message);
   }
-  const updatedItem = storage.updateItem({
+  console.log(`Updating shopping list item \`${req.params.id}\``);
+  const updatedItem = ShoppingList.update({
     id: req.params.id,
     name: req.body.name,
-    budget: req.body.budget
+    checked: req.body.checked
   });
   res.status(204).json(updatedItem);
-})
+});
 
+// when DELETE request comes in with an id in path,
+// try to delete that item from ShoppingList.
+app.delete('/shopping-list/:id', (req, res) => {
+  ShoppingList.delete(req.params.id);
+  console.log(`Deleted shopping list item \`${req.params.ID}\``);
+  res.status(204).end();
+});
+
+
+app.get('/recipes', (req, res) => {
+  res.json(Recipes.get());
+});
+
+app.post('/recipes', jsonParser, (req, res) => {
+  // ensure `name` and `budget` are in request body
+  const requiredFields = ['name', 'ingredients'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const item = Recipes.create(req.body.name, req.body.ingredients);
+  res.status(201).json(item);
+});
+
+// when PUT request comes in with updated recipe, ensure has
+// required fields. also ensure that recipe id in url path, and
+// recipe id in updated item object match. if problems with any
+// of that, log error and send back status code 400. otherwise
+// call `Recipes.updateItem` with updated recipe.
+app.put('/recipes/:id', jsonParser, (req, res) => {
+  const requiredFields = ['name', 'ingredients', 'id'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id `
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating shopping list item \`${req.params.id}\``);
+  const updatedItem = Recipes.update({
+    id: req.params.id,
+    name: req.body.name,
+    ingredients: req.body.ingredients
+  });
+  res.status(204).json(updatedItem);
+});
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
