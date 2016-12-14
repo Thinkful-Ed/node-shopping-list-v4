7ad9bc8eb2601eef286358c584caaee80f959617
Benjamin E White <benjamin.e.white1@gmail.com>
2016-12-14 16:13:18 -0500

    Remove solution from starter file

--- server.js
+++ server.js
@@ -107,36 +107,6 @@ app.post('/recipes', jsonParser, (req, res) => {
   res.status(201).json(item);
 });
 
-// when PUT request comes in with updated recipe, ensure has
-// required fields. also ensure that recipe id in url path, and
-// recipe id in updated item object match. if problems with any
-// of that, log error and send back status code 400. otherwise
-// call `Recipes.updateItem` with updated recipe.
-app.put('/recipes/:id', jsonParser, (req, res) => {
-  const requiredFields = ['name', 'ingredients', 'id'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
-      console.error(message);
-      return res.status(400).send(message);
-    }
-  }
-  if (req.params.id !== req.body.id) {
-    const message = (
-      `Request path id (${req.params.id}) and request body id `
-      `(${req.body.id}) must match`);
-    console.error(message);
-    return res.status(400).send(message);
-  }
-  console.log(`Updating shopping list item \`${req.params.id}\``);
-  const updatedItem = Recipes.update({
-    id: req.params.id,
-    name: req.body.name,
-    ingredients: req.body.ingredients
-  });
-  res.status(204).json(updatedItem);
-});
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
