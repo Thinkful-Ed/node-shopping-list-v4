a445164a5bbf36a8944fe16dd970a41fb28c3e3b
Benjamin E White <benjamin.e.white1@gmail.com>
2016-12-16 13:03:27 -0500

    Add recipes delete back in

--- server.js
+++ server.js
@@ -107,6 +107,11 @@ app.post('/recipes', jsonParser, (req, res) => {
   res.status(201).json(item);
 });
 
+app.delete('/recipes/:id', (req, res) => {
+  Recipes.delete(req.params.id);
+  console.log(`Deleted recipe \`${req.params.ID}\``);
+  res.status(204).end();
+});
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
